a4aeab8d3e518338d178cc96783dd05439418111
redbloodareme <maiducmeo@gmail.com>
2026-02-07 16:21:37 +0700

    Update main_menu.js

--- src/main_menu.js
+++ src/main_menu.js
@@ -1,20 +1,11 @@
-console.log("MAIN MENU JS RUNNING");
-
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "white";
 ctx.font = "48px Arial";
+ctx.fillText("HELLO WORLD", 400, 360);
 
-function loop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = "white";
-  ctx.fillText("MAIN MENU", 480, 360);
-
-  requestAnimationFrame(loop);
-}
-
-loop();
+console.log("DRAW OK");
